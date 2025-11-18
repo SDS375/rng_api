4e7ac8b6ae767616d51150a920fa24a7dcd37bf3
Roger D. Peng <rdpeng@gmail.com>
2025-11-17 20:53:34 -0600

    Add message

--- api_endpoint.R
+++ api_endpoint.R
@@ -2,6 +2,9 @@
 #*
 #* <DELETE ME: add HTTP method, end point name, and parameter name>
 function(  ) {   ## Add a parameter name to the function
+    message("getting random number!")
+
     ## Return a random number after setting the seed
+
 }
 
